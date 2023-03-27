79a36d0a3769041a50dc7cb07cd649fee6a68e9f
alexserrani <129045244+alexserrani@users.noreply.github.com>
2023-03-27 11:29:37 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,20 @@ $controller_prefix = 'App\Http\Controllers\\';
 
 // This is where you want to define your path
 // and put appropriate middleware to protect your route
-// we  want to prevent unauthenticate user to go to "/" route
+// we want to prevent unauthenticated user to go to "/" route
 // if you have more time
-// craete a middleware to prevent authenticated user from "/login" and "/register"
+// create a middleware to prevent authenticated user from "/login" and "/register"
+
+Route::group(['middleware' => ['auth.redirect']], function () use ($controller_prefix) {
+    Route::get('/login', $controller_prefix.'AuthController@login');
+    Route::get('/register', $controller_prefix.'AuthController@register');
+});
+
+Route::post('/login', $controller_prefix.'AuthController@authenticate')->name('login');
+Route::post('/register', $controller_prefix.'AuthController@register')->name('register');
+Route::post('/logout', $controller_prefix.'AuthController@logout')->name('logout');
 
 Route::get('/', function () {
     //TODO
     return view('index');
 })->middleware('auth');
-
-Route::match(array('GET', 'POST'), 'login', $controller_prefix.'AuthController@login')->name('login');
-Route::match(array('GET', 'POST'), 'register', $controller_prefix.'AuthController@register')->name('register');
