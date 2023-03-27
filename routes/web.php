906eff7fc9917c1aabe4d38cd8e5cf6f69697f29
alexserrani <129045244+alexserrani@users.noreply.github.com>
2023-03-27 13:32:22 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,17 @@
-<?php
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TriviaController;
 use Illuminate\Support\Facades\Route;
 
-$controller_prefix = 'App\Http\Controllers\\';
+Route::get('/', [TriviaController::class, 'index'])->name('home');
 
-// This is where you want to define your path
-// and put appropriate middleware to protect your route
-// we  want to prevent unauthenticate user to go to "/" route
-// if you have more time
-// craete a middleware to prevent authenticated user from "/login" and "/register"
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [TriviaController::class, 'dashboard'])->name('dashboard');
+});
 
-Route::get('/', function () {
-    //TODO
-    return view('index');
-})->middleware('auth');
+Route::prefix('auth')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.show');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register.show');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+});
 
-Route::match(array('GET', 'POST'), 'login', $controller_prefix.'AuthController@login')->name('login');
-Route::match(array('GET', 'POST'), 'register', $controller_prefix.'AuthController@register')->name('register');
