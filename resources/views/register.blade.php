c53a406af82f14c2ba829f926850f22d339c0742
alexserrani <129045244+alexserrani@users.noreply.github.com>
2023-03-27 10:54:36 +0200

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,22 +5,22 @@
     <div class="container">
         <div class="wrapper">
             <form class="auth-container" method="POST" action="{{ route('register') }}">
-				{{ csrf_field() }}
+                {{ csrf_field() }}
 
                 <div class="page-title">
                     <h2>SIGN UP</h2>
                 </div>
 
                 <div class="form-container">
-                    <input type="text" placeholder="username" required>
+                    <input type="text" placeholder="username" required name="username">
                 </div>
 
                 <div class="form-container">
-                    <input type="password" placeholder="password" required>
+                    <input type="password" placeholder="password" required name="password">
                 </div>
 
                 <div class="form-container">
-                    <input type="password" placeholder="confirm password" required>
+                    <input type="password" placeholder="confirm password" required name="password_confirmation">
                 </div>
 
                 <button class="button-submit" type="submit">Signup</button>
