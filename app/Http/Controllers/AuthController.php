a4c9aa6f96667421a8943040a79ab3f5196f04a8
alexserrani <129045244+alexserrani@users.noreply.github.com>
2023-03-27 11:03:11 +0200

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,29 +1,39 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login() {
-        // Input constraint
-        // username must be unique, min length of 6 , max length of 30, and is required
-        // password is required, min length of 6 and max length of 30
-        // password confirmation is required and must be same as password field
-        // show error whenever needed if any of this constraint got violated
+    public function login()
+    {
         return view('login');
-
     }
 
-    public function register(Request $request) {
-        // Input constraint:
-        // -username must be unique, min length of 6 , max length of 30, and is required
-        // -password is required, min length of 6 and max length of 30
-        // -password confirmation is required and must be same as password field
-        // If all constraint is satisfied, hash the password
-        // store the password and the username in users table.
-        // show error whenever needed if any of this constraint got violated
+   public function register(Request $request)
+{
+    $validatedData = $request->validate([
+        'username' => 'required|unique:users|max:30|min:6',
+        'password' => 'required|max:30|min:6',
+        'password_confirmation' => 'required|same:password',
+    ], [
+        'username.required' => 'The username field is required.',
+        'username.unique' => 'The username is already taken.',
+        'username.max' => 'The username may not be greater than 30 characters.',
+        'username.min' => 'The username must be at least 6 characters.',
+        'password.required' => 'The password field is required.',
+        'password.max' => 'The password may not be greater than 30 characters.',
+        'password.min' => 'The password must be at least 6 characters.',
+        'password_confirmation.required' => 'The password confirmation field is required.',
+        'password_confirmation.same' => 'The password confirmation does not match.',
+    ]);
 
-        return view('register');
-    }
+    $user = new User();
+    $user->username = $validatedData['username'];
+    $user->password = bcrypt($validatedData['password']);
+    $user->save();
+
+    return redirect('/login');
 }
+
